5456d722d196da2ab9f6fc0b0b0edd0951280796
Dan Christensen <816.dan@gmail.com>
2017-08-14 14:29:02 +0000

    Added a semi-functional login page.

--- the-data-express/index.js
+++ the-data-express/index.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var mongoose = require('mongoose');
+var express = require('express'),
+    mongoose = require('mongoose'),
+    pug = require('pug'),
+    path = require('path'),
+    bodyParser = require('body-parser');
 mongoose.Promise = require('bluebird');
 
 var path = require('path');
@@ -7,6 +10,7 @@ var path = require('path');
 var routesController = require('./controllers/routes.js');
 
 var app = express();
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
@@ -17,5 +21,14 @@ var router = express.Router();
 router.get('/', routesController.home);
 
 app.use('/', router);
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.post('/login', urlencodedParser, function (req, res) {
+    console.log(req.body.userName);  
+    res.render('landing', req.body);
+});
 
 app.listen(3000);
