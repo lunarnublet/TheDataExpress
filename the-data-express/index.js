44a2dc80b709812a353bafc3b8a2f2650b23571d
jouzune <dmatson@student.neumont.edu>
2017-08-15 08:05:44 -0600

    Admin page

--- the-data-express/index.js
+++ the-data-express/index.js
@@ -1,20 +1,32 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/data');
 mongoose.Promise = require('bluebird');
 
 var path = require('path');
 
+var config = require("./config.json");
+var cookies = require("./util/cookie.js");
+
 var homeController = require('./controllers/home.js');
+var adminController = require('./controllers/admin.js');
 
 var app = express();
-
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
+app.use(cookieParser());
+app.use(expressSession({
+    secret: 'the-data-express',
+    saveUninitialized: true,
+    resave: true
+}));
 app.use(express.static(path.join(__dirname + '/public')));
 
 var router = express.Router();
-
 router.get('/', homeController.home);
+router.get('/admin', authenticate, adminController.control);
 
 app.use('/', router);
 
