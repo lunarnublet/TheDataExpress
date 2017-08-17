236ecbc9792d8e3dfe6825442c39a547f8e2665f
jouzune <dmatson@student.neumont.edu>
2017-08-17 14:17:55 -0600

    Added repository modules

--- the-data-express/controllers/routes.js
+++ the-data-express/controllers/routes.js
@@ -1,32 +1,25 @@
 var config = require("../config.json");
-var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
-var expressSession = require('express-session');
-mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb://localhost/data');
+var models;
 
-var mdb = mongoose.connection;
-mdb.on('error', console.error.bind(console, 'connection error:'));
-mdb.once('open', function (callback) {
+module.exports = function(repository) {
+    models = repository;
 
-});
+    return {
+        home: home,
+        register: register,
+        registerPost: registerPost,
+        admin: admin,
+        login: login,
+        loginPost: loginPost,
+        editUser: editUser,
+        editUserPost: editUserPost,
+        deleteUser: deleteUser,
+    };
+}
 
-var userSchema = mongoose.Schema({
-    username: String,
-    password: String,
-    age: String,
-    email: String,
-    role: String,
-    answer1: String,
-    answer2: String,
-    answer3: String
-});
-
-var User = mongoose.model('User_Accounts', userSchema);
-
-exports.home = function (req, res) {
-    seedDatabase();
-    User.find("username", function (err, users) {
+var home = function (req, res) {
+    models.Users.find({}, function (err, users) {
         if (err) return console.error(err);
         var ans1 = [];
         var strings1 = [];
@@ -89,106 +82,27 @@ exports.home = function (req, res) {
         for (var i = 0; i < ans1.length; ++i) {
             ans1[i] = (ans1[i] / users.length) * 100;
         }
-        for (var i = 0; i < ans1.length; ++i) {
+        for (var i = 0; i < ans2.length; ++i) {
             ans2[i] = (ans2[i] / users.length) * 100;
         }
-        for (var i = 0; i < ans1.length; ++i) {
+        for (var i = 0; i < ans3.length; ++i) {
             ans3[i] = (ans3[i] / users.length) * 100;
         }
         res.render("index", {
             config: config, ans1: ans1, ans2: ans2, ans3: ans3, answerNames1: strings1, answerNames2: strings2, answerNames3: strings3,
+            questions: config.questions,
             userSession: req.session.user,
             time: req.cookies.time,                            
         });
     });
 }
-function deleteAllUsers() {
-    console.log("DELETEALLUSERS()");
-    // var allUsers = User.find({});
-    // allUsers.exec().then(function (doc) {
-    //     doc.forEach(function(user) {
-    //       console.log("found user", user);  
-    //     });
-    // });
-    var users = User.find({});
-    users.exec().then(function (doc) {
-        if (doc) {
-            doc.forEach(function (user) {
-                var remove = User.remove(user);
-                remove.exec().then(function (res) {
-                    console.log("  Deleted user: " + user.username);
-                })
-            });
-        }
-    });
-}
 
-// do this after all validation checks
-// data must have a non-hashed password
-function registerUser(data) {
-    bcrypt.hash(data.password, null, null, function (err, hashedPass) {
-        if (err) return false;
 
-        var user = new User({
-            username: data.username,
-            password: hashedPass,
-            role: data.role,
-            email: data.email,
-            age: data.age,
-            answer1: data.answer1,
-            answer2: data.answer2,
-            answer3: data.answer3
-        });
-        user.save(function (err, user) {
-            if (err) return false;
-            return true;
-        });
-    });
-}
-
-function seedDatabase() {
-    var query = User.findOne({ username: "admin" });
-    query.exec().then(function (doc) {
-        if (doc) {
-            return;
-        }
-
-        var admin = {
-            username: "admin",
-            password: "pass",
-            role: "admin",
-            email: "admin@express.com",
-            age: 28,
-            answer1: "3",
-            answer2: "2",
-            answer3: "1",
-        };
-        registerUser(admin);
-        for (var i = 1; i < 11; ++i) {
-            var user = {
-                username: "user" + i,
-                password: "pass",
-                age: 25,
-                email: "user" + i + "@express.com",
-                role: "user",
-                answer1: Math.floor((Math.random() * 4) + 1),
-                answer2: Math.floor((Math.random() * 4) + 1),
-                answer3: Math.floor((Math.random() * 4) + 1)
-            };
-            registerUser(user);
-            // user.save(function (err, user) {
-            //     if (err) return console.error(err);
-            //     console.log(user.username + ' added');
-            //   });
-        }
-    });
-}
-
-exports.register = function (req, res) {
+var register = function (req, res) {
     res.render("register", { config: config, title: "Register" });
 }
-exports.registerPost = function (req, res) {
-    var query = User.find({ username: req.body.userName });
+var registerPost = function (req, res) {
+    var query = models.Users.find({ username: req.body.userName });
     query.exec().then(function (users) {
         if (users.length) {
             // name taken
@@ -198,41 +112,28 @@ exports.registerPost = function (req, res) {
         res.redirect("/login");
     });
 }
-exports.login = function (req, res) {
+var login = function (req, res) {
     res.render("login", {
         config: config,
         title: "Login Page",
     });
 }
-exports.loginPost = function (req, res, next) {
-    User.findOne({ username: req.body.userName }, function (err, user) {
-        if (err) {
-            console.log('User.findOne failed');
+var loginPost = function (req, res, next) {
+    models.tryLoginUser(req.body.userName, req.body.password, function (user) {
+        console.log(user);
+    
+        if (user) {
+            req.session.user = user;
+            delete req.session.user.password;
+            res.redirect('/');
+        } else {
             res.redirect('/login');
-        }
-        else {
-            console.log(user);
-            bcrypt.compare(req.body.password, user.password, function (err, isValidPassword) {
-                if (err) {
-                    console.log('bcrypt.Compare failed');
-                    res.redirect('/login');
-                }
-                else {
-                    if (isValidPassword) {
-                        req.session.user = user;
-                        delete req.session.user.password;
-                        res.redirect('/');
-                    } else {
-                        res.redirect('/login');
-                    }
-                }
-            });
         }
     });
 }
 
-exports.admin = function (req, res) {
-    var query = User.find({});
+var admin = function (req, res) {
+    var query = models.Users.find({});
     query.exec().then(function (doc) {
         if (doc) {
             res.render('admin', {
@@ -247,7 +148,7 @@ exports.admin = function (req, res) {
     });
 }
 
-exports.deleteUser = function (req, res, next) {
+var deleteUser = function (req, res, next) {
     var userId = req.query.id;
 
     var query = User.findOneAndRemove({ _id: userId });
@@ -258,20 +159,22 @@ exports.deleteUser = function (req, res, next) {
     });
 }
 
-exports.editUser = function (req, res, next) {
+var editUser = function (req, res, next) {
     var userId = req.params.id ? req.params.id : req.session.user._id;
-    
+
     // only let admins or the same user edit a user's information    
     if (req.session.user.role === "admin" ||
         req.session.user._id === userId) {
 
-        var query = User.findOne({ _id: userId });
+        var query = models.Users.findOne({ _id: userId });
         query.exec().then(function (val) {
+            console.log("editUser:GET\n", val);
             res.render("edit", {
                 userToEdit: val,
                 userSession: req.session.user,
                 time: req.cookies.time,            
-                config: config
+                config: config,
+                questions: config.questions
             });
         }, function (reason) {
             console.log(reason);
@@ -281,7 +184,7 @@ exports.editUser = function (req, res, next) {
         res.sendStatus(403);
     }
 }
-exports.editUserPost = function (req, res, next) {
+var editUserPost = function (req, res, next) {
     var userId = req.body.id;
 
     // only let admins or the same user edit a user's information
@@ -297,38 +200,14 @@ exports.editUserPost = function (req, res, next) {
         if (req.body.answer2) info.answer2 = req.body.answer2;
         if (req.body.answer3) info.answer3 = req.body.answer3;
 
-        if (info.password) {
-            bcrypt.hash(info.password, null, null, function (err, hashed) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    info.password = hashed;
-                    var query = User.findOneAndUpdate({ _id: userId }, info);
-                    query.exec().then(function (val) {
-                        req.session.user.role = info.role ? info.role : req.session.user.role;
-                        res.render("edit", {
-                            userToEdit: info,
-                            user: req.session.user,
-                            time: req.cookies.time,                
-                            config: config
-                        });
-                    });
-                }
-            });
-        } else {
-            var query = User.findOneAndUpdate({ _id: userId }, info);
-            query.exec().then(function (val) {
-                req.session.user.role = info.role ? info.role : req.session.user.role;
-                res.render("edit", {
-                    userToEdit: info,
-                    userSession: req.session.user,
-                    time: req.cookies.time,                                    
-                    config: config
-                });
-            });
-        }
+        models.editUser(userId, info, function (user) {
+            if (req.session.user._id === userId) {
+                req.session.user.role = user.role;          
+            }
+            console.log("editUser:POST\n", user);
+            res.redirect('/');
+        });
     } else {
         res.sendStatus(403);
     }
-
 }
---
+++ the-data-express/repository/repository.js
@@ -0,0 +1,118 @@
+var bcrypt = require('bcrypt-nodejs');
+var models;
+module.exports = function(mongooseModels) {
+    models = mongooseModels;
+    // deleteAllUsers();
+    seedDatabase();
+
+    models.tryLoginUser = tryLoginUser;
+    models.registerUser = registerUser;
+    models.editUser = editUser;
+
+    return models;
+}
+var editUser = function(id, info, callback) {
+    if (info.password) {
+        info.password = bcrypt.hashSync(info.password, null);
+    }
+    var query = models.Users.findOneAndUpdate({ _id: id }, info);
+    query.exec().then(function (val) {
+        callback(info);                
+    }, function(err) {
+        console.log(err);
+    });
+}
+
+var tryLoginUser = function(userName, password, callback) {
+    var query = models.Users.findOne({ username: userName });
+    
+    query.exec().then(function(user){
+        var isValidPassword = bcrypt.compareSync(password, user.password);
+        console.log("valid password", isValidPassword);
+        isValidPassword ? callback(user) : callback(undefined);
+    });
+}
+
+// do this after all validation checks
+// data must have a non-hashed password
+var registerUser = function(data) {
+    bcrypt.hash(data.password, null, null, function (err, hashedPass) {
+        if (err) return false;
+
+        var user = new models.Users({
+            username: data.username,
+            password: hashedPass,
+            role: data.role,
+            email: data.email,
+            age: data.age,
+            answer1: data.answer1,
+            answer2: data.answer2,
+            answer3: data.answer3
+        });
+        user.save(function (err, user) {
+            if (err) return false;
+            return true;
+        });
+    });
+}
+
+function deleteAllUsers() {
+    console.log("DELETEALLUSERS()");
+    var allUsers = models.Users.find({});
+    allUsers.exec().then(function (doc) {
+        if (doc) {
+            doc.forEach(function(user) {
+            console.log("found user", user);  
+            });
+        }
+    });
+    var users = models.Users.find({});
+    users.exec().then(function (doc) {
+        if (doc) {
+            doc.forEach(function (user) {
+                var remove = models.Users.remove(user);
+                remove.exec().then(function (res) {
+                    console.log("  Deleted user: " + user.username);
+                })
+            });
+        }
+    });
+}
+
+function seedDatabase() {
+    var query = models.Users.findOne({ username: "admin" });
+    query.exec().then(function (doc) {
+        if (doc) {
+            return;
+        }
+
+        var admin = {
+            username: "admin",
+            password: "pass",
+            role: "admin",
+            email: "admin@express.com",
+            age: 28,
+            answer1: "4",
+            answer2: "4",
+            answer3: "4",
+        };
+        registerUser(admin);
+        for (var i = 1; i < 11; ++i) {
+            var user = {
+                username: "user" + i,
+                password: "pass",
+                age: 25,
+                email: "user" + i + "@express.com",
+                role: "user",
+                answer1: Math.floor((Math.random() * 4) + 1),
+                answer2: Math.floor((Math.random() * 4) + 1),
+                answer3: Math.floor((Math.random() * 4) + 1)
+            };
+            registerUser(user);
+            // user.save(function (err, user) {
+            //     if (err) return console.error(err);
+            //     console.log(user.username + ' added');
+            //   });
+        }
+    });
+}
